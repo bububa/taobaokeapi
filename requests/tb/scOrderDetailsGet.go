bc8a08b2a0f605c6dbced1698230851aaf16e10b
Syd Xu <prof.syd.xu@gmail.com>
2022-02-23 14:10:57 +0800

    feat: add ScOrderDetailsGetResponse

--- requests/tb/scOrderDetailsGet.go
+++ requests/tb/scOrderDetailsGet.go
@@ -63,3 +63,18 @@ func (r ScOrderDetailsGetRequest) Params() url.Values {
 	}
 	return params
 }
+
+type ScOrderDetailsResponse struct {
+	// PublisherOrderDto
+	Results []PublisherOrderDto `json:"results,omitempty" xml:"results>publisher_order_dto,omitempty"`
+	// 位点字段，由调用方原样传递
+	PositionIndex string `json:"position_index,omitempty" xml:"position_index,omitempty"`
+	// 页码
+	PageNo int `json:"page_no,omitempty" xml:"page_no,omitempty"`
+	// 页大小
+	PageSize int `json:"page_size,omitempty" xml:"page_size,omitempty"`
+	// 是否还有上一页
+	HasPre bool `json:"has_pre,omitempty" xml:"has_pre,omitempty"`
+	// 是否还有下一页
+	HasNext bool `json:"has_next,omitempty" xml:"has_next,omitempty"`
+}
